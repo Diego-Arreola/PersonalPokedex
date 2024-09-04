ae30a1c10bc5bb65b70b9d3cc49131a4db500abf
Diego-Arreola <diegoarreolahernandez66@gmail.com>
2024-09-04 09:36:48 -0600

    "pagination"

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   const [pokemon, setPokemon] = useState([]);
   
   useEffect(() => {
-    getPokemonList();
+    getPokemonList(151,0);
   }, []);
 
-  const getPokemonList = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const getPokemonList = (limit, offset) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)      
       .then(response => response.json())
       .then(data => setPokemon(data.results))
   }
@@ -31,14 +31,17 @@ function App() {
         <img id="pokemon_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"></img>
         <h1>Personal Pokedex</h1>
       </div>
-      {/* <div className="pokedexdisplay"></div>
-      <button onClick={getPokemonList}>Catch pokemon</button>
-      <ul className="pokemonList">
-        {pokemon.map((pokemon, index) => {
-          return <li key={index}>{pokemon.name}, {pokemon.url}</li>
-        })}
-      </ul>
-      <button onClick={getPokemon('https://pokeapi.co/api/v2/pokemon/1')}>HOLAA</button> */}
+      <div  className="generations">
+        <button onClick={() => getPokemonList(151)}>Generation 1</button>
+        <button onClick={() => getPokemonList(100,151)}>Generation 2</button>
+        <button onClick={() => getPokemonList(135,251)}>Generation 3</button>
+        <button onClick={() => getPokemonList(107,386)}>Generation 4</button>
+        <button onClick={() => getPokemonList(156,493)}>Generation 5</button>
+        <button onClick={() => getPokemonList(72,649)}>Generation 6</button>
+        <button onClick={() => getPokemonList()}>Generation 7</button>
+        <button onClick={() => getPokemonList()}>Generation 8</button>
+        <button onClick={() => getPokemonList()}>Generation 9</button>
+      </div>
       <div className="pokedexEntrys">
         {pokemon.map((pokemon, index) => {
           return <PokedexEntry key={index} data={pokemon} />
